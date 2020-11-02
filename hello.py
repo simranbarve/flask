66f2cdbdf486362ec16fe00aa66dbab292b5ca9b
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-11-02 08:52:14 +0400

    created login page

--- hello.py
+++ hello.py
@@ -3,4 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return render_template('index.html', new = arr)
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    return render_template('login.html')
