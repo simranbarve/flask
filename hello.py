78298f7b50c968ad62e5b0e8ba4498f4e8672811
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-11-05 11:53:17 +0400

    Post and Get implemented with a form

--- hello.py
+++ hello.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import (Flask, render_template, url_for, request)
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    n = len(arr)
-    for i in range(n - 1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def home():
+    return render_template('index.html')
 
-    return render_template('index.html', new = arr)
-
-@app.route('/login', methods=["POST", "GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        lst = request.form["nums"]
+        return redirect(url_for("list", list=lst))
+    else:
+        return render_template("login.html")
+
+@app.route("/<lst>")
+def user(list):
+    n = len(list)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if list[j] > list[j+1] :
+                list[j], list[j+1] = list[j+1], list[j]
+    return f"<h1>{list}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
